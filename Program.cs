57fd94c5139a43bdcf4db7c4d176f82d11d72455
Mnishthakur <102964888+Mnishthakur@users.noreply.github.com>
2023-04-23 19:19:53 +0530

    Add : UC-1

--- Program.cs
+++ Program.cs
@@ -1,8 +1,27 @@
 namespace GenericsProblems;
 class Program
 {
-    static void Main()
+   using System;
+
+namespace GenericsProblems
+{
+    class Program
     {
+        static void Main()
+        {
+            Console.WriteLine("Test case 1: Max number is at first position");
+            int max1 = FindMax(10, 2, 1);
+            Console.WriteLine("Max number is: " + max1); // Output: Max number is: 10
+
+            Console.WriteLine("Test case 2: Max number is at second position");
+            int max2 = FindMax(5, 53, 2);
+            Console.WriteLine("Max number is: " + max2); // Output: Max number is: 53
+
+            Console.WriteLine("Test case 3: Max number is at third position");
+            int max3 = FindMax(10, 3, 14);
+            Console.WriteLine("Max number is: " + max3); // Output: Max number is: 14
+        }
+
         static int FindMax(int a, int b, int c)
         {
             int max = int.MinValue;
@@ -20,18 +39,5 @@ class Program
             }
             return max;
         }
-        // Test case 1: Max number is at first position
-        int max1 = FindMax(9, 2, 1);
-        Console.WriteLine("Max number is: " + max1); // Output: Max number is: 10
-
-        // Test case 2: Max number is at second position
-        int max2 = FindMax(25, 53, 68);
-        Console.WriteLine("Max number is: " + max2); // Output: Max number is: 8
-
-        // Test case 3: Max number is at third position
-        int max3 = FindMax(10, 3, 4);
-        Console.WriteLine("Max number is: " + max3); // Output: Max number is: 7
     }
-
-
 }
