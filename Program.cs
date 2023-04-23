91a1ce53af16a84c7fde3d62754d4f75dd79dafe
Mnishthakur <102964888+Mnishthakur@users.noreply.github.com>
2023-04-23 19:32:36 +0530

    Add: Refactor-1 Program.cs

---
+++ Program.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace GenericsProblems
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Test case 1: Max number is at first position");
+            int maxInt1 = FindMax<int>(9, 2, 1);
+            Console.WriteLine("Max number is: " + maxInt1); // Output: Max number is: 9
+
+            Console.WriteLine("Test case 2: Max number is at second position");
+            float maxFloat2 = FindMax<float>(25.5f, 53.2f, 68.9f);
+            Console.WriteLine("Max number is: " + maxFloat2); // Output: Max number is: 68.9
+
+            Console.WriteLine("Test case 3: Max string is at third position");
+            string maxString3 = FindMax<string>("apple", "banana", "cherry");
+            Console.WriteLine("Max string is: " + maxString3); // Output: Max string is: cherry
+        }
+
+        static T FindMax<T>(T a, T b, T c) where T : IComparable
+        {
+            T max = a;
+            if (b.CompareTo(max) > 0)
+            {
+                max = b;
+            }
+            if (c.CompareTo(max) > 0)
+            {
+                max = c;
+            }
+            return max;
+        }
+    }
+}
+
