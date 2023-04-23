53df061923939ece8865ca4b8726bb773b049ce3
Mnishthakur <102964888+Mnishthakur@users.noreply.github.com>
2023-04-23 19:26:42 +0530

    Add: UC-3 Program.cs

---
+++ Program.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace GenericsProblems
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Test case 1: Max string is at first position");
+            string max1 = FindMax("Apple", "Peach", "Banana");
+            Console.WriteLine("Max string is: " + max1); // Output: Max string is: Peach
+
+            Console.WriteLine("Test case 2: Max string is at second position");
+            string max2 = FindMax("Dog", "Elephant", "Cat");
+            Console.WriteLine("Max string is: " + max2); // Output: Max string is: Elephant
+
+            Console.WriteLine("Test case 3: Max string is at third position");
+            string max3 = FindMax("Green", "Red", "Yellow");
+            Console.WriteLine("Max string is: " + max3); // Output: Max string is: Yellow
+        }
+
+        static string FindMax(string a, string b, string c)
+        {
+            string max = null;
+            if (a.CompareTo(max) > 0)
+            {
+                max = a;
+            }
+            if (b.CompareTo(max) > 0)
+            {
+                max = b;
+            }
+            if (c.CompareTo(max) > 0)
+            {
+                max = c;
+            }
+            return max;
+        }
+    }
+}
