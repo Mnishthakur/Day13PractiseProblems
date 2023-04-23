95363a7d08421aeb62a674f0ded2266fb09c4bd1
Mnishthakur <102964888+Mnishthakur@users.noreply.github.com>
2023-04-23 19:41:30 +0530

    Add: UC-5 Program.cs

---
+++ Program.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace GenericsProblems
+{
+    class Program
+    {
+        static void Main()
+        {
+            TestMaximum(9, 2, 1);
+            TestMaximum(25.5f, 53.2f, 68.9f, -3.4f);
+            TestMaximum("apple", "banana", "cherry", "date");
+        }
+
+        static void TestMaximum<T>(T a, T b, params T[] args) where T : IComparable
+        {
+            T max = FindMax(a, b, args);
+            PrintMax(max);
+        }
+
+        static T FindMax<T>(T a, T b, params T[] args) where T : IComparable
+        {
+            T[] arr = new T[args.Length + 2];
+            arr[0] = a;
+            arr[1] = b;
+            Array.Copy(args, 0, arr, 2, args.Length);
+            Array.Sort(arr);
+            return arr[^1];
+        }
+
+        static void PrintMax<T>(T max)
+        {
+            Console.WriteLine("Max value is: " + max);
+        }
+    }
+}
